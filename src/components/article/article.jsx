f50ef1ab85fa339ecb52d698e253ee2bc87c04c2
cescontreras <contreras-cesaralejandro@hotmail.com>
2020-12-13 12:05:55 -0300

    url link

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -3,10 +3,13 @@ import trash from "../../asset/trash.png";
 import "./article.css";
 
 export default function Article({ article, deleteArticle }) {
+	
 	return (
 		<div className="article">
 			<div className="main-data">
-				<div className="article-title">{article.title}</div>
+				<div className="article-title">
+					<a href={article.url}>{article.title}</a>					
+				</div>
 				<div className="article-author">- {article.author} - </div>
 			</div>
 			<div className="article-date">
