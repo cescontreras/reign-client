27a54976855fb06f7ce6322b49e2d4012b718536
cescontreras <contreras-cesaralejandro@hotmail.com>
2020-12-13 13:23:23 -0300

    update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function Header() {
 	return (
 		<header className="App-header">
-			<h1>HN Feed</h1>
-			<p>{"We <3 hackerNews"}</p>
+			<div className="header-content">
+				<h1>HN Feed</h1>
+				<p>{"We <3 hackerNews"}</p>
+			</div>
 		</header>
 	);
 }
